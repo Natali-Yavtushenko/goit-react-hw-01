6a548b1ab1271b0d9710e6ef6f095d11d06532a0
unknown <yavtushenkon@gmail.com>
2024-12-06 19:56:21 +0200

    profilecss

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,29 +7,29 @@ avatar="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
 })
 {
 return (
-<div className="container">
-  <div className="usercontainer">
-    <img className="avatar"
+<div className={css.container}>
+  <div className={css.usercontainer}>
+    <img className={css.avatar}
       src={avatar}
       alt="User avatar"
       />
-      <p className="username">{username}</p>
-      <p className="usertag">{tag}</p>
-      <p className="userlocation">{location}</p>
+      <p className={css.username}>{username}</p>
+      <p className={css.usertag}>{tag}</p>
+      <p className={css.userlocation}>{location}</p>
        </div>
 
-      <ul className="userstatslist">
-      <li className="userstatsitem">
+      <ul className={css.userstatslist}>
+      <li className={css.userstatsitem}>
      <span>Followers</span>
-      <span>{stats.followers}</span>
+      <span className={css.statsspan}>{stats.followers}</span>
       </li>
-      <li className="statsitem">
+      <li className={css.userstatsitem}>
      <span>Views</span>
-      <span>{stats.views}</span>
+      <span className={css.statsspan}>{stats.views}</span>
       </li>
-      <li className="statsitem">
+      <li className={css.userstatsitem}>
       <span>Likes</span>
-      <span>{stats.likes}</span>
+      <span className={css.statsspan}>{stats.likes}</span>
     </li>
   </ul>
 </div> )}
